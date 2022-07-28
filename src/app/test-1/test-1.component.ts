e9135917fb3582a788683f738e9007231a4bb29c
HumbertoMarinao <humberto.mv@gmail.com>
2022-07-28 19:56:19 -0400

    Initial commit

---
+++ src/app/test-1/test-1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'humberto-comp',
+  templateUrl: './test-1.component.html',
+  styleUrls: ['./test-1.component.css']
+})
+export class Test1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
